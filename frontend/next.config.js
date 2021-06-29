c8b977b543a982eddcb8f6bcd863a9cd3bc0a8a3
blait455 <blait455@gmail.com>
2021-06-29 02:22:19 +0100

    auth with jwt

---
+++ frontend/next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  publicRuntimeConfig: {
+    APP_NAME: 'SEO-BLOG',
+    API_DEVELOPMENT: 'http://localhost:8000/api',
+    API_PRODUCTION: 'https://domain-name.com/api',
+    PRODUCTION: false
+  }
+}
