81a5982e8a7f33a8dd5ffaf8cc845ba1fe8583aa
blait455 <blait455@gmail.com>
2021-07-02 04:15:51 +0100

    sending emails with sendgrid & user and site contact forms

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -46,6 +46,12 @@ const Header = () => {
                   <NavLink style={{ cursor: 'pointer' }}>Blogs</NavLink>
                 </Link>
               </NavItem>
+
+              <NavItem>
+                <Link href="/contact">
+                  <NavLink style={{ cursor: 'pointer' }}>Contact</NavLink>
+                </Link>
+              </NavItem>
             </React.Fragment>
 
             {!isAuth() && (
